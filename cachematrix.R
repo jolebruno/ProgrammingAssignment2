f0ba2ffdb2a62339ad7636e9878dd6f8e8222334
jolebruno <jolandabruno@virgilio.it>
2023-06-28 17:16:29 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## for this assignment the <<- operator is introduced to assign a value to an object in 
+# an environment different from the current one. 
+# makeCacheMatrix creates a matrix that can cash itself and get and set the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    invmatrix <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) invmatrix <<- solve
+  getinverse <- function() invmatrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve computes the inverse of the matrix we created with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invmatrix <- x$getinverse()
+  if(!is.null(invmatrix)) {
+    message("getting cached data")
+    return(invmatrix)
+  }
+  data <- x$get()
+  invmatrix <- solve(data, ...)
+  x$setinverse(invmatrix)
+  invmatrix
+  
 }
+
+# now I test if my function works 
+
+my_matrix <- makeCacheMatrix()
+
+my_matrix$set(matrix(1:4,2,2))
+
+my_matrix$get()
+
+cacheSolve(my_matrix)
+
+my_matrix$getinverse()
